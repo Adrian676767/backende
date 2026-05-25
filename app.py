ee8563d66a2665698dd340b82f6d258c7833a2a2
Adrian676767 <adriancervenka517@gmail.com>
2026-05-25 07:50:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 students = [
     {"id": 1, "name": "Samuel Oravec", "age": 18, "vyska": 175},
